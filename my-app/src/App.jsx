70a11f7914f2768cf991c8879c9fc393083300be
zarko stanisic <zarko@tcom.rs>
2019-08-13 10:24:24 +0200

    state

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -25,63 +25,83 @@ import React from 'react'
 //   }
 // }
 
+// class App extends React.Component{
+//   render(){
+//     return(
+//       <div>
+//         <Header/>
+//         <Content/>
+//       </div>
+//     );
+//   }
+// }
+//
+// class Header extends React.Component{
+//   render(){
+//     return(
+//       <div>
+//         <h1>Header</h1>
+//       </div>
+//     );
+//   }
+// }
+//
+// class Content extends React.Component{
+//   constructor(){
+//     super();
+//
+//     this.state = {
+//       data: [
+//         {'id': 1, 'name': 'Foo', 'age': 20},
+//         {'id': 2, 'name': 'Bar', 'age': 30},
+//         {'id': 3, 'name': 'Baz', 'age': 40}
+//       ]
+//     }
+//   }
+//
+//   render(){
+//     return(
+//       <div>
+//         <h2>Content</h2>
+//         <table>
+//           <tbody>
+//             {this.state.data.map((person, i) => <TableRow key = {i} data = {person}/>)}
+//           </tbody>
+//         </table>
+//       </div>
+//     );
+//   }
+// }
+//
+// class TableRow extends React.Component{
+//   render(){
+//     return(
+//       <div>
+//         <tr>
+//           <td>{this.props.data.id}</td>
+//           <td>{this.props.data.name}</td>
+//           <td>{this.props.data.age}</td>
+//         </tr>
+//       </div>
+//     );
+//   }
+// }
+
 class App extends React.Component{
-  render(){
-    return(
-      <div>
-        <Header/>
-        <Content/>
-      </div>
-    );
-  }
-}
-
-class Header extends React.Component{
-  render(){
-    return(
-      <div>
-        <h1>Header</h1>
-      </div>
-    );
-  }
-}
-
-class Content extends React.Component{
-  constructor(){
-    super();
+  constructor(props){
+    super(props);
 
     this.state = {
-      data: [
-        {'id': 1, 'name': 'Foo', 'age': 20},
-        {'id': 2, 'name': 'Bar', 'age': 30},
-        {'id': 3, 'name': 'Baz', 'age': 40}
-      ]
+      header: 'Header from state...',
+      content: 'Content from state...'
     }
   }
-  
-  render(){
-    return(
-      <div>
-        <h2>Content</h2>
-        <table>
-          <tbody>
-            {this.state.data.map((person, i) => <TableRow key = {i} data = {person}/>)}
-          </tbody>
-        </table>
-      </div>
-    );
-  }
-}
 
-class TableRow extends React.Component{
   render(){
     return(
       <div>
-        <tr>
-          <td>{this.props.data.id}</td>
-          <td>{this.props.data.name}</td>
-          <td>{this.props.data.age}</td>
-        </tr>
+        <h1>{this.state.header}</h1>
+        <h2>{this.state.content}</h2>
       </div>
     );
   }
