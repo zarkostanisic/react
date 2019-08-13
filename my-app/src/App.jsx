e59fa2f89d919c2b770db0f41e0ff94f4c9e5e90
zarko stanisic <zarko@tcom.rs>
2019-08-13 10:01:27 +0200

    jsx

---
+++ my-app/src/App.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+class App extends React.Component{
+  // render(){
+  //   var i = 1;
+  //
+  //   var myStyle = {
+  //     fontSize: 100,
+  //     color: '#FF0000'
+  //   }
+  //
+  //   return(
+  //     <div>
+  //        Hello World!!!
+  //       <h1>Header</h1>
+  //       <h2>Content</h2>
+  //       <p>This is the content!!!</p>
+  //       <p data-myattribute="somevalue">This is the content!!!</p>
+  //       <h1>{1 + 1}</h1>
+  //       <h1>{i == 1 ? 'True!' : 'False!'}</h1>
+  //       <h1 style={myStyle}>Style</h1>
+  //       {/*End of the line Comment*/}
+  //     </div>
+  //   );
+  // }
+}
+
+export default App;
