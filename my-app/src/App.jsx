e9dd2b2f326b11a786204268f2ee49f2ccc70944
zarko stanisic <zarko@tcom.rs>
2019-08-22 14:01:42 +0200

    events

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -321,44 +321,84 @@ import ReactDOM from 'react-dom';
 // }
 
 // 9.
-class App extends React.Component{
-  constructor(props){
-    super(props);
+// class App extends React.Component{
+//   constructor(props){
+//     super(props);
+//
+//     this.state = {
+//       data: 'Initial data...'
+//     }
+//
+//     this.updateState = this.updateState.bind(this);
+//   }
+//
+//   updateState(e){
+//     this.setState({data: e.target.value});
+//   }
+//
+//   render(){
+//     return(
+//       <div>
+//        {/*
+//          <input type="text" value= {this.state.data}
+//             onChange = {this.updateState}
+//           />
+//           <h4>{this.state.data}</h4>
+//         */}
+//         <Content myDataProp = {this.state.data} updateStateProp = {this.updateState}></Content>
+//       </div>
+//     );
+//   }
+// }
+//
+// class Content extends React.Component{
+//   render(){
+//     return(
+//       <div>
+//         <input type="text" value = {this.props.myDataProp} onChange = {this.props.updateStateProp}/>
+//         <h3>{this.props.myDataProp}</h3>
+//       </div>
+//     );
+//   }
+// }
 
-    this.state = {
-      data: 'Initial data...'
-    }
-
-    this.updateState = this.updateState.bind(this);
-  }
-
-  updateState(e){
-    this.setState({data: e.target.value});
-  }
-
-  render(){
-    return(
-      <div>
-       {/*
-         <input type="text" value= {this.state.data}
-            onChange = {this.updateState}
-          />
-          <h4>{this.state.data}</h4>
-        */}
-        <Content myDataProp = {this.state.data} updateStateProp = {this.updateState}></Content>
-      </div>
-    );
-  }
-}
-
-class Content extends React.Component{
-  render(){
-    return(
-      <div>
-        <input type="text" value = {this.props.myDataProp} onChange = {this.props.updateStateProp}/>
-        <h3>{this.props.myDataProp}</h3>
-      </div>
-    );
-  }
-}
+// 10.
+// class App extends React.Component{
+//   constructor(props){
+//     super(props);
+//
+//     this.state = {
+//       data: 'Initial data...'
+//     }
+//
+//     this.updateState = this.updateState.bind(this);
+//   };
+//
+//   updateState(){
+//     this.setState({data: 'Data updated...'});
+//   }
+//
+//   render(){
+//     return(
+//       <div>
+//         {/*
+//         <button onClick= {this.updateState}>CLICK</button>
+//         <h4>{this.state.data}</h4>
+//         */}
+//         <Content myDataProp = {this.state.data} updateStateProp = {this.updateState}></Content>
+//       </div>
+//     );
+//   }
+// }
+//
+// class Content extends React.Component{
+//   render(){
+//     return(
+//       <div>
+//         <button onClick = {this.props.updateStateProp}>CLICK</button>
+//         <h3>{this.props.myDataProp}</h3>
+//       </div>
+//     );
+//   }
+// }
 export default App;
