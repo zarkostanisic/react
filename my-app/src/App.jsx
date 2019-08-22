dafb72057ae151e5feacd93131b5ac19d9c96a8e
zarko stanisic <zarko@tcom.rs>
2019-08-22 14:05:17 +0200

    events

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -363,42 +363,42 @@ import ReactDOM from 'react-dom';
 // }
 
 // 10.
-// class App extends React.Component{
-//   constructor(props){
-//     super(props);
-//
-//     this.state = {
-//       data: 'Initial data...'
-//     }
-//
-//     this.updateState = this.updateState.bind(this);
-//   };
-//
-//   updateState(){
-//     this.setState({data: 'Data updated...'});
-//   }
-//
-//   render(){
-//     return(
-//       <div>
-//         {/*
-//         <button onClick= {this.updateState}>CLICK</button>
-//         <h4>{this.state.data}</h4>
-//         */}
-//         <Content myDataProp = {this.state.data} updateStateProp = {this.updateState}></Content>
-//       </div>
-//     );
-//   }
-// }
-//
-// class Content extends React.Component{
-//   render(){
-//     return(
-//       <div>
-//         <button onClick = {this.props.updateStateProp}>CLICK</button>
-//         <h3>{this.props.myDataProp}</h3>
-//       </div>
-//     );
-//   }
-// }
+class App extends React.Component{
+  constructor(props){
+    super(props);
+
+    this.state = {
+      data: 'Initial data...'
+    }
+
+    this.updateState = this.updateState.bind(this);
+  };
+
+  updateState(){
+    this.setState({data: 'Data updated...'});
+  }
+
+  render(){
+    return(
+      <div>
+        {/*
+        <button onClick= {this.updateState}>CLICK</button>
+        <h4>{this.state.data}</h4>
+        */}
+        <Content myDataProp = {this.state.data} updateStateProp = {this.updateState}></Content>
+      </div>
+    );
+  }
+}
+
+class Content extends React.Component{
+  render(){
+    return(
+      <div>
+        <button onClick = {this.props.updateStateProp}>CLICK</button>
+        <h3>{this.props.myDataProp}</h3>
+      </div>
+    );
+  }
+}
 export default App;
