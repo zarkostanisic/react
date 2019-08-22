c61bd5db0a0e9a37a2a43b58b56519d2a1814de9
zarko stanisic <zarko@tcom.rs>
2019-08-22 12:10:08 +0200

    component life cycle

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -255,4 +255,69 @@ import ReactDOM from 'react-dom';
 //     );
 //   }
 // }
+
+// 8.
+class App extends React.Component{
+    constructor(props){
+      super(props);
+
+      this.state = {
+        data: 0
+      }
+
+      this.setNewNumber = this.setNewNumber.bind(this);
+    };
+
+    setNewNumber(){
+      this.setState({data: this.state.data + 1});
+    }
+
+    render(){
+      return(
+        <div>
+          <button onClick = {this.setNewNumber}>INCREMENT</button>
+          <Content myNumber = {this.state.data}></Content>
+        </div>
+      );
+    }
+}
+
+class Content extends React.Component{
+  componentWillMount(){
+    console.log('Component WILL MOUNT!');
+  }
+
+  componentDidMount(){
+    console.log('Component DID MOUNT!');
+  }
+
+  componentWillReceiveProps(newProps){
+    console.log('Component WILL RECEIVE PROPS!');
+  }
+
+  shouldComponentUpdate(newProps, newState){
+    return true;
+  }
+
+  componentWillUpdate(nextProps, nextState){
+    console.log('Component WILL UPDATE!');
+  }
+
+  componentDidUpdate(prevProps, nextState){
+    console.log('Component DID UPDATE!');
+  }
+
+  componentWillUnmount(){
+    console.log('Component WIILL UNMOUNT!');
+  }
+
+  render(){
+    return(
+      <div>
+        <h3>{this.props.myNumber}</h3>
+      </div>
+    );
+  }
+}
+
 export default App;
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -5,3 +5,6 @@ import App from './App.jsx'
 
 ReactDOM.render(<App/>, document.getElementById('root'));
 // ReactDOM.render(<App headerProp="Header from props..." contentProp="Content from props..."/>, document.getElementById('root'));
+setTimeout(() => {
+  ReactDOM.unmountComponentAtNode(document.getElementById('root'));
+}, 1000);
