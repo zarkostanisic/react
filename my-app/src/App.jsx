6b24e9cf45d01c970d1453d7ed4b2388a7289526
zarko stanisic <zarko@tcom.rs>
2019-08-22 14:35:21 +0200

    refs

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -403,34 +403,34 @@ import ReactDOM from 'react-dom';
 // }
 
 // 11.
-// class App extends React.Component{
-//   constructor(props){
-//     super(props);
-//
-//     this.state = {
-//       data: ''
-//     }
-//
-//     this.updateState = this.updateState.bind(this);
-//     this.clearInput = this.clearInput.bind(this);
-//   }
-//
-//   updateState(e){
-//     this.setState({data: e.target.value});
-//   }
-//
-//   clearInput(){
-//     this.setState({data: ''});
-//     ReactDOM.findDOMNode(this.refs.myInput).focus();
-//   }
-//
-//   render(){
-//     return(
-//       <div>
-//         <input value = {this.state.data} onChange = {this.updateState} ref = "myInput"/>
-//         <button onClick = {this.clearInput}>CLEAR</button>
-//       </div>
-//     );
-//   }
-// }
+class App extends React.Component{
+  constructor(props){
+    super(props);
+
+    this.state = {
+      data: ''
+    }
+
+    this.updateState = this.updateState.bind(this);
+    this.clearInput = this.clearInput.bind(this);
+  }
+
+  updateState(e){
+    this.setState({data: e.target.value});
+  }
+
+  clearInput(){
+    this.setState({data: ''});
+    ReactDOM.findDOMNode(this.refs.myInput).focus();
+  }
+
+  render(){
+    return(
+      <div>
+        <input value = {this.state.data} onChange = {this.updateState} ref = "myInput"/>
+        <button onClick = {this.clearInput}>CLEAR</button>
+      </div>
+    );
+  }
+}
 export default App;
