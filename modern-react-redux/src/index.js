38dec812d0ab87f6176d81a79d9a5e848a09358d
zarko stanisic <zarko@tcom.rs>
2019-08-28 16:42:50 +0200

    modern-react-redux section2-7

--- modern-react-redux/src/index.js
+++ modern-react-redux/src/index.js
@@ -2,9 +2,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+function getButtonText(){
+  return 'Click on me!';
+}
+
 // Create a react component
 const App = () => {
-  return <div>Hi there!</div>
+  const buttonText = ['Hi', 'There'];
+  const style = {backgroundColor: 'blue', color: 'white'};
+  const labelText = 'Enter name:';
+
+  return <div>
+      <label  className="label" for="name">{labelText}</label>
+      <input id="name" type="text"/>
+      <button style={style}>
+        {buttonText}
+        {/*getButtonText()*/}
+      </button>
+    </div>;
 }
 
 // Take the react component and show it on the screen
