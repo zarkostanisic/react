4a20e20aa3b9a06500d1e4bf4eebd3af763c247f
zarko stanisic <zarko@tcom.rs>
2019-08-28 11:31:35 +0200

    w3 forms

--- w3/src/index.js
+++ w3/src/index.js
@@ -120,8 +120,88 @@ class Football extends React.Component{
   }
 }
 
+class MyForm extends React.Component{
+  constructor(props){
+    super(props);
+
+    this.state = {
+      username: '',
+      age: null,
+      errorMessage: '',
+      description: 'The content of a textarea goes in the value attribute',
+      brand: 'Volvo'
+    };
+  }
+
+  mySubmitHandler = (event) => {
+    event.preventDefault();
+    let username = this.state.username;
+    let age = this.state.age;
+
+    // if(!Number(age)){
+    //   alert('Your age must be a number');
+    // }
+
+    alert('You are submitting ' + username + ' - ' + age);
+  }
+
+  myChangeHandler = (event) => {
+    let nam = event.target.name;
+    let val = event.target.value;
+    let err = '';
+
+    if(nam === 'age'){
+      if(val != '' && !Number(val)){
+        err = <strong>Your age must be a number</strong>;
+      }
+    }
+
+    this.setState({errorMessage: err});
+    this.setState({[nam]: val});
+  }
+
+  render(){
+    let header = '';
+
+    if(this.state.username){
+      header = <h1>Hello {this.state.username}</h1>;
+    }else{
+      header = '';
+    }
+
+    return(
+      <form onSubmit={this.mySubmitHandler}>
+        {header}
+        <h1>Hello {this.state.username} {this.state.age}</h1>
+        <p>Enter your name:</p>
+        <input type="text"
+          name="username"
+          onChange={this.myChangeHandler}
+        />
+        <p>Enter your age:</p>
+        <input type="text"
+          name="age"
+          onChange={this.myChangeHandler}
+        />
+        <p>Description:</p>
+        <textarea value={this.state.description}></textarea>
+        <p>Brand:</p>
+        <select value={this.state.brand}>
+          <option value="Ford">Ford</option>
+          <option value="Volvo">Volvo</option>
+          <option value="Fiat">Fiat</option>
+        </select>
+        <br/>
+        <input type="submit"/>
+        <br/>
+        {this.state.errorMessage}
+      </form>
+    );
+  }
+}
+
 // ReactDOM.render(myElement, document.getElementById('root'));
 ReactDOM.render(
-  <div><Garage size="big"/><Header favcol="yellow"/><Container/><Football/></div>,
+  <div>{/*<Garage size="big"/><Header favcol="yellow"/><Container/><Football/>*/}<MyForm/></div>,
    document.getElementById('root')
  );
