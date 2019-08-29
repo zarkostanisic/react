d6a51e63086a2a035b807ea822e039a94c0bb858
zarko stanisic <zarko@tcom.rs>
2019-08-29 16:28:00 +0200

    modern-react-redux section6-10

--- modern-react-redux/src/components/seasons/SeasonDisplay.jsx
+++ modern-react-redux/src/components/seasons/SeasonDisplay.jsx
@@ -1,4 +1,16 @@
 import React from 'react';
+import './SeasonDisplay.css';
+
+const seasonConfig = {
+  summer: {
+    text: "Let's hit the beach.",
+    iconName: 'sun',
+  },
+  winter: {
+    text: 'Brurr, it is cold!',
+    iconName: 'snowflake',
+  }
+};
 
 const getSeason = (lat, month) => {
   if(month > 2 && month < 9){
@@ -11,14 +23,13 @@ const getSeason = (lat, month) => {
 class SeasonDisplay extends React.Component {
   render(){
     const season = getSeason(this.props.lat, new Date().getMonth());
-    const text = season === 'summer' ? 'Lets hit the beach' : 'Brurr, it is chilly';
-    const icon = season === 'summer' ? 'sun' : 'snowflake';
+    const {text, iconName} = seasonConfig[season];
 
     return(
-      <div>
-        <i className={icon + ' icon'}/>
+      <div className={`season-display ${season}`}>
+        <i className={`icon-left massive ${iconName} icon`}/>
         <h1>{text}</h1>
-        <i className={icon + ' icon'}/>
+        <i className={`icon-right massive ${iconName} icon`}/>
       </div>
     );
   }
