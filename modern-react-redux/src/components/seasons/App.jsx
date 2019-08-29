13de8da04061de43fb87f78e04c4d92d4edc6c5a
zarko stanisic <zarko@tcom.rs>
2019-08-29 14:17:36 +0200

    modern-react-redux section5-6

--- modern-react-redux/src/components/seasons/App.jsx
+++ modern-react-redux/src/components/seasons/App.jsx
@@ -7,27 +7,28 @@ class App extends React.Component {
 
       this.state = {
         lat: null,
-        lon: null
+        errorMessage: ''
       }
 
       window.navigator.geolocation.getCurrentPosition(
         (position) => {
           console.log(position);
           this.setState({lat: position.coords.latitude});
-          this.setState({lon: position.coords.longitude});
         },
-        (err) => console.log(err)
+        (err) => {
+          this.setState({errorMessage: err.message});
+        }
       );
   }
 
   render(){
-    return(
-      <div>
-        <div>Latitude: {this.state.lat}</div>
-        <div>Longitude: {this.state.lon}</div>
-        <SeasonDisplay/>
-      </div>
-    );
+    if(this.state.errorMessage && !this.state.lat){
+      return <div>Error: {this.state.errorMessage}</div>;
+    }else if(!this.state.errorMessage && this.state.lat){
+      return <div>Latitude: {this.state.lat}</div>
+    }
+
+    return <div>Loading</div>
   }
 }
 
