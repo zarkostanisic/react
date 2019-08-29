151b91246ee04a47616ee077ff62afcdec04b359
zarko stanisic <zarko@tcom.rs>
2019-08-29 12:54:49 +0200

    modern-react-redux section4-7

--- modern-react-redux/src/components/seasons/App.jsx
+++ modern-react-redux/src/components/seasons/App.jsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import SeasonDisplay from './SeasonDisplay'
 
-const App = () => {
-  window.navigator.geolocation.getCurrentPosition(
-    (position) => console.log(position),
-    (err) => console.log(err)
-  );
-  return(
-    <div>
-      <SeasonDisplay/>
-    </div>
-  );
+class App extends React.Component {
+  render(){
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => console.log(position),
+      (err) => console.log(err)
+    );
+    
+    return(
+      <div>
+        <div>Latitude:</div>
+        <SeasonDisplay/>
+      </div>
+    );
+  }
 }
 
 export default App;
