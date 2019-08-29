71777b8f327145281bd3003589e0698ba9e45f4c
zarko stanisic <zarko@tcom.rs>
2019-08-29 15:01:14 +0200

    modern-react-redux section6-4

--- modern-react-redux/src/components/seasons/App.jsx
+++ modern-react-redux/src/components/seasons/App.jsx
@@ -2,24 +2,34 @@ import React from 'react';
 import SeasonDisplay from './SeasonDisplay'
 
 class App extends React.Component {
-  constructor(props){
-      super(props);
+  // constructor(props){
+  //     super(props);
+  //
+  //     this.state = {
+  //       lat: null,
+  //       errorMessage: ''
+  //     }
+  // }
 
-      this.state = {
-        lat: null,
-        errorMessage: ''
-      }
-
-      window.navigator.geolocation.getCurrentPosition(
-        (position) => {
-          console.log(position);
-          this.setState({lat: position.coords.latitude});
-        },
-        (err) => {
-          this.setState({errorMessage: err.message});
-        }
-      );
+  state = {
+    lat: null,
+    errorMessage: ''
   }
+
+  componentDidMount(){
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => this.setState({lat: position.coords.latitude}),
+      (err) => this.setState({errorMessage: err.message})
+    );
+  }
+
+  // componentDidUpdate(){
+  //   console.log('DID UPDATE');
+  // }
+
+  // componentWillUnmount(){
+  //   console.log('WILL UNMOUNT');
+  // }
 
   render(){
     if(this.state.errorMessage && !this.state.lat){
