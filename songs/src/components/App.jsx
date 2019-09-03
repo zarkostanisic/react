b12ce199015653cf7329e6afca49bf3e1eb9ac77
zarko stanisic <zarko@tcom.rs>
2019-09-03 15:39:59 +0200

    modern-react-redus section13-6

---
+++ songs/src/components/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {selectSong} from '../actions/index';
+
+class App extends React.Component{
+  render(){
+    return(
+      <div>App</div>
+    );
+  }
+}
+
+export default App;
---
+++ songs/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Songs - Section 13
+import App from './components/App';
+
+ReactDOM.render(
+  <App/>,
+  document.querySelector('#root')
+);
