6c98f6b92a7d98f562a84c76cd9bb29bdaacdc53
zarko stanisic <zarko@tcom.rs>
2019-09-04 10:09:44 +0200

    modern-react-redux section13-17

--- songs/src/components/SongDetail.jsx
+++ songs/src/components/SongDetail.jsx
@@ -3,8 +3,22 @@ import {connect} from 'react-redux';
 
 class SongDetail extends Component{
   render(){
+    const song = this.props.song;
+    if(song){
+      return(
+        <div>
+          <h3>Details for:</h3>
+          <p>
+            {song.title}
+            <br/>
+            {song.duration}
+          </p>
+        </div>
+      );
+    }
+
     return(
-      <div>SongDetail</div>
+      <div>Select a song</div>
     );
   }
 }
