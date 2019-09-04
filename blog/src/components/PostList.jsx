f17718628fce96d49a07886307d7edbd491c3749
zarko stanisic <zarko@tcom.rs>
2019-09-04 11:07:24 +0200

    modern-react-redux section14-6

--- blog/src/components/PostList.jsx
+++ blog/src/components/PostList.jsx
@@ -1,6 +1,11 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {fetchPosts} from '../actions';
 
 class PostList extends Component{
+  componentDidMount(){
+    this.props.fetchPosts();
+  }
   render(){
     return(
       <div>PostList</div>
@@ -8,4 +13,6 @@ class PostList extends Component{
   }
 }
 
-export default PostList;
+export default connect(null, {
+  fetchPosts: fetchPosts
+})(PostList);
