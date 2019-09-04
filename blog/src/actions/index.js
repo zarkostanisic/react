66c55360679669e4ae8c4cb3f8f1563caba29755
zarko stanisic <zarko@tcom.rs>
2019-09-04 13:47:39 +0200

    modern-react-redux section14-12

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,10 +1,7 @@
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 
-export const fetchPosts = () => {
-  const promise = jsonPlaceholder.get('/posts');
+export const fetchPosts = () => async dispatch =>{
+    const promise = await jsonPlaceholder.get('/posts');
 
-  return{
-    type: 'FETCH_POSTS',
-    payload: promise
-  }
+    dispatch({type: 'FETCH_POSTS',payload: promise});
 }
