4c5189d563517348d43cb18bba60e37aeb11cdef
zarko stanisic <zarko@tcom.rs>
2019-09-05 14:55:27 +0200

    modern-react-redux section15-22

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -9,10 +9,24 @@ export const fetchPosts = () => async dispatch =>{
 }
 
 // Fetch user
-export const fetchUser = (id) => dispatch => _fetchUser(id, dispatch);
-
-const _fetchUser = _.memoize(async (id, dispatch) => {
+// 1.
+export const fetchUser = (id) => async dispatch =>{
   const repsonse = await jsonPlaceholder.get('/users/' + id);
 
   dispatch({type: 'FETCH_USER', payload: repsonse.data});
-});
+}
+
+// 2.
+// export const fetchUser = (id) => dispatch => _fetchUser(id, dispatch);
+//
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+//   const repsonse = await jsonPlaceholder.get('/users/' + id);
+//
+//   dispatch({type: 'FETCH_USER', payload: repsonse.data});
+// });
+
+export const fetchPostsAndUsers = () => async dispatch => {
+  console.log('About to fetch posts');
+  await dispatch(fetchPosts());
+  console.log('Fetched posts!');
+}
--- blog/src/components/PostList.jsx
+++ blog/src/components/PostList.jsx
@@ -1,12 +1,13 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
-import {fetchPosts} from '../actions';
+import {/*fetchPosts,*/ fetchPostsAndUsers} from '../actions';
 
 import UserHeader from './UserHeader';
 
 class PostList extends Component{
   componentDidMount(){
-    this.props.fetchPosts();
+    // this.props.fetchPosts();
+    this.props.fetchPostsAndUsers();
   }
 
   renderList(){
@@ -42,5 +43,6 @@ const mapStateToProps = (state) => {
 }
 
 export default connect(mapStateToProps, {
-  fetchPosts: fetchPosts
+  // fetchPosts: fetchPosts,
+  fetchPostsAndUsers: fetchPostsAndUsers
 })(PostList);
