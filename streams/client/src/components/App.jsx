7118840c55b8f13c1c202e4e92615bac3d03fd76
zarko stanisic <zarko@tcom.rs>
2019-09-05 16:17:11 +0200

    modern-react-redux section16-6

--- streams/client/src/components/App.jsx
+++ streams/client/src/components/App.jsx
@@ -1,10 +1,29 @@
 import React, {Component} from 'react';
+import {BrowserRouter, Route} from 'react-router-dom';
+
+const PageOne = () => {
+  return <div>PageOne</div>;
+}
+
+const PageTwo = () => {
+  return (
+    <div>
+      PageTwo
+      <button>Click Me!</button>
+    </div>
+  );
+}
 
 class App extends Component{
   render(){
     return(
       <div>
-          App
+        <div>
+          <BrowserRouter>
+            <Route path="/" exact component={PageOne}/>
+            <Route path="/pagetwo" component={PageTwo}/>
+          </BrowserRouter>
+        </div>
       </div>
     );
   }
