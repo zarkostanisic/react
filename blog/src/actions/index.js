12c9f68f15e39096b26ca1fd89df7ceccff5fdc2
zarko stanisic <zarko@tcom.rs>
2019-09-05 14:11:24 +0200

    modern-react-redux section15-20

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,12 +1,14 @@
 import _ from 'lodash';
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 
+// Fetch posts
 export const fetchPosts = () => async dispatch =>{
     const repsonse = await jsonPlaceholder.get('/posts');
 
     dispatch({type: 'FETCH_POSTS',payload: repsonse});
 }
 
+// Fetch user
 export const fetchUser = (id) => dispatch => _fetchUser(id, dispatch);
 
 const _fetchUser = _.memoize(async (id, dispatch) => {
