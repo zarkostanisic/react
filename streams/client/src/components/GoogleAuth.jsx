5784a762f14f6f6f54b9a18660ed3e5992bb2d94
zarko stanisic <zarko@tcom.rs>
2019-09-06 15:44:39 +0200

    modern-react-redux section17-12

--- streams/client/src/components/GoogleAuth.jsx
+++ streams/client/src/components/GoogleAuth.jsx
@@ -1,4 +1,6 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {signIn, signOut} from '../actions';
 
 class GoogleAuth extends Component{
   state = {
@@ -19,8 +21,12 @@ class GoogleAuth extends Component{
     });
   }
 
-  onAuthChange = () => {
-    this.setState({isSignedIn: this.auth.isSignedIn.get()});
+  onAuthChange = (isSignedIn) => {
+    if(!isSignedIn){
+      this.props.signIn();
+    }else{
+      this.props.signOut();
+    }
   }
 
   onSignIn = () => {
@@ -58,4 +64,7 @@ class GoogleAuth extends Component{
   }
 }
 
-export default GoogleAuth;
+export default connect(null, {
+  signIn: signIn,
+  signOut: signOut
+})(GoogleAuth);
