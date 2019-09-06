c506cfb08ee0d3be09ce692eb1e41af409a076d0
zarko stanisic <zarko@tcom.rs>
2019-09-06 15:09:26 +0200

    modern-react-redux section17-8

--- streams/client/src/components/GoogleAuth.jsx
+++ streams/client/src/components/GoogleAuth.jsx
@@ -14,22 +14,36 @@ class GoogleAuth extends Component{
       }).then(() => {
         this.auth = window.gapi.auth2.getAuthInstance();
         this.setState({isSignedIn: this.auth.isSignedIn.get()});
+        this.auth.isSignedIn.listen(this.onAuthChange);
       });
     });
+  }
+
+  onAuthChange = () => {
+    this.setState({isSignedIn: this.auth.isSignedIn.get()});
   }
 
   renderAuthButton(){
     if(this.state.isSignedIn === null){
       return <div>I dont know if we are signed in</div>;
     }else if(this.state.isSignedIn === true){
-      return <div>I am signed in!</div>;
+      return(
+        <button className="ui red google button">
+          <i className="google icon"/>
+          Sign Out
+        </button>
+      );
     }else{
-      return <div>I am not signed in</div>;
+      return (
+        <button className="ui blue google button">
+          <i className="google icon"/>
+          Sign In with Google
+        </button>
+      );
     }
   }
 
   render(){
-    console.log(this.state.isSignedIn);
     return(
       <div>{this.renderAuthButton()}</div>
     );
