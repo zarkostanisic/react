a9619d1ef2bdb579172226510bb37d8bade2d44f
zarko stanisic <zarko@tcom.rs>
2019-09-09 14:17:11 +0200

    modern-react-redux section20-10

--- streams/client/src/reducers/streamReducer.js
+++ streams/client/src/reducers/streamReducer.js
@@ -10,16 +10,37 @@ const INITIAL_STATE = {
 
 };
 
-export default (state = INITIAL_STATE, action) => {
+// const streamReducer (state = INITIAL_STATE, action) => {
+//   switch(action.type){
+//     case EDIT_STREAM:
+//       return state.map(stream => {
+//         if(stream.id === action.payload.id){
+//           return action.payloadl
+//         }else{
+//           return stream;
+//         }
+//       });
+//     default:
+//       return state;
+//   }
+// }
+
+export default  (state = {}, action) => {
   switch(action.type){
+    case CREATE_STREAM:
+     return {...state, [action.payload.id]: action.payload};
+    case FETCH_STREAMS:
+      return;
+    case FETCH_STREAM:
+      return {...state, [action.payload.id]: action.payload};
+    case DELETE_STREAM:
+     return;
     case EDIT_STREAM:
-      return state.map(stream => {
-        if(stream.id === action.payload.id){
-          return action.payloadl
-        }else{
-          return stream;
-        }
-      });
+      // const newState = {...state}
+      // newState[action.payload.id] = action.payload;
+      // return newState;
+
+      return {...state, [action.payload.id]: action.payload};
     default:
       return state;
   }
