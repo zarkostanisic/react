01e885a1c2773e7af7e503c72209c81dd39dca00
zarko stanisic <zarko@tcom.rs>
2019-09-09 14:52:34 +0200

    modern-react-redux section20-14

--- streams/client/src/components/streams/StreamList.jsx
+++ streams/client/src/components/streams/StreamList.jsx
@@ -1,13 +1,47 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {fetchStreams} from '../../actions';
 
 class StreamList extends Component{
+  componentDidMount(){
+    this.props.fetchStreams();
+  }
+
+  renderList = () => {
+    return this.props.streams.map(stream => {
+      return(
+        <div className="item" key={stream.id}>
+          <i className="large middle aligned icon camera"/>
+          <div className="content">
+            {stream.title}
+            <div className="description">
+              {stream.description}
+            </div>
+          </div>
+        </div>
+      );
+    });
+  }
   render(){
+    console.log(this.props.streams);
+
     return(
       <div>
-        StreamList
+        <h2>Streams</h2>
+        <div className="ui celled list">
+          {this.renderList()}
+        </div>
       </div>
     );
   }
 }
 
-export default StreamList;
+const mapStateToProps = (state) => {
+  return {
+    streams: Object.values(state.streams)
+  }
+}
+
+export default connect(mapStateToProps, {
+  fetchStreams: fetchStreams
+})(StreamList);
