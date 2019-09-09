c09cae8fa325598559ec2ef4bd839cb70e3bec43
zarko stanisic <zarko@tcom.rs>
2019-09-09 10:38:52 +0200

    modern-react-redux section19-6

--- streams/client/src/components/streams/StreamCreate.jsx
+++ streams/client/src/components/streams/StreamCreate.jsx
@@ -1,11 +1,23 @@
 import React, {Component} from 'react';
+import {Field, reduxForm} from 'redux-form';
 
 class StreamCreate extends Component{
+  renderInput({input}){
+    return <input {...input}/>;
+  }
+
   render(){
     return(
-      <div>StreamCreate</div>
+      <div>
+        <form>
+          <Field name="title" component={this.renderInput}/>
+          <Field name="description" component={this.renderInput}/>
+        </form>
+      </div>
     );
   }
 }
 
-export default StreamCreate;
+export default reduxForm({
+  form: 'streamCreate'
+})(StreamCreate);
