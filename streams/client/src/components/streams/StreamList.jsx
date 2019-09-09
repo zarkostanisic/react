0f28261910cf2207f6b42f0cee8b1acae51b9935
zarko stanisic <zarko@tcom.rs>
2019-09-09 15:30:31 +0200

    modern-react-redux section20-18

--- streams/client/src/components/streams/StreamList.jsx
+++ streams/client/src/components/streams/StreamList.jsx
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
+import {Link} from 'react-router-dom';
 import {fetchStreams} from '../../actions';
 
 class StreamList extends Component{
@@ -25,7 +26,9 @@ class StreamList extends Component{
           {this.renderAdmin(stream)}
           <i className="large middle aligned icon camera"/>
           <div className="content">
-            {stream.title}
+            <div className="header">
+              {stream.title}
+            </div>
             <div className="description">
               {stream.description}
             </div>
@@ -34,6 +37,19 @@ class StreamList extends Component{
       );
     });
   }
+
+  renderCreate(){
+    if(this.props.isSignedIn){
+      return(
+        <div style={{textAlign: 'right'}}>
+          <Link to="/streams/new" className="ui button primary">
+            CREATE
+          </Link>
+        </div>
+      )
+    }
+  }
+
   render(){
     return(
       <div>
@@ -41,6 +57,7 @@ class StreamList extends Component{
         <div className="ui celled list">
           {this.renderList()}
         </div>
+        {this.renderCreate()}
       </div>
     );
   }
@@ -49,7 +66,8 @@ class StreamList extends Component{
 const mapStateToProps = (state) => {
   return {
     streams: Object.values(state.streams),
-    currentUserId: state.auth.userId
+    currentUserId: state.auth.userId,
+    isSignedIn: state.auth.isSignedIn
   }
 }
 
