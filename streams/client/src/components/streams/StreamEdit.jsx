f2a2f39dbd43b150f277e7ac48cbee82056aab58
zarko stanisic <zarko@tcom.rs>
2019-09-10 09:37:42 +0200

    modern-react-redux section20-30

--- streams/client/src/components/streams/StreamEdit.jsx
+++ streams/client/src/components/streams/StreamEdit.jsx
@@ -1,12 +1,30 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {fetchStream} from '../../actions';
 
 class StreamEdit extends Component{
+  componentDidMount(){
+    this.props.fetchStream(this.props.match.params.id);
+  }
+
   render(){
-    console.log(this.props.match.params.id);
-    return(
-      <div>StreamEdit</div>
-    );
+    if(this.props.stream){
+      return(
+        <div>{this.props.stream.title}</div>
+      );
+    }
+
+    return <div>Loading...</div>
   }
 }
 
-export default StreamEdit;
+const mapStateToProps = (state, ownProps) => {
+  console.log(ownProps);
+  return {
+    stream: state.streams[ownProps.match.params.id]
+  }
+}
+
+export default connect(mapStateToProps, {
+  fetchStream: fetchStream
+})(StreamEdit);
