53a7efd67b2e02fda5278ec694483a5bee60709a
zarko stanisic <zarko@tcom.rs>
2019-09-10 12:06:25 +0200

    modern-react-redux section20-35

--- streams/client/src/actions/index.js
+++ streams/client/src/actions/index.js
@@ -45,9 +45,7 @@ export const fetchStream = (id) => async dispatch => {
 }
 
 export const editStream = (id, formValues) => async (dispatch, getState) => {
-  const {userId} = getState().auth;
-  formValues.userId = userId;
-  const response = await streams.put('/streams/' + id, formValues);
+  const response = await streams.patch('/streams/' + id, formValues);
 
   dispatch({type: EDIT_STREAM, payload: response.data});
 
