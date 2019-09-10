97b03530e5a4ee78aee0e21815fcf52f4a763410
zarko stanisic <zarko@tcom.rs>
2019-09-10 16:07:01 +0200

    modern-react-redux section21-6

--- streams/client/src/components/streams/StreamDelete.jsx
+++ streams/client/src/components/streams/StreamDelete.jsx
@@ -1,9 +1,20 @@
 import React, {Component} from 'react';
+import Modal from '../Modal';
 
 class StreamDelete extends Component{
   render(){
+    const actions = (
+      <React.Fragment>
+        <button className="ui button negative">Delete</button>
+        <button className="ui button">Cancel</button>
+      </React.Fragment>
+    );
+
+
     return(
-      <div>StreamDelete</div>
+      <div>
+        <Modal title="Delete Stream" content="Are you sure you want to delete this stream?" actions={actions}/>
+      </div>
     );
   }
 }
