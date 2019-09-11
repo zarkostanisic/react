076bc3170a53cb7daba5e18b2f5cda4847b732df
zarko stanisic <zarko@tcom.rs>
2019-09-11 10:49:49 +0200

    modern-react-redux section22-13

--- streams/client/src/components/streams/StreamShow.jsx
+++ streams/client/src/components/streams/StreamShow.jsx
@@ -1,11 +1,45 @@
 import React, {Component} from 'react';
+import flv from 'flv.js';
 import {connect} from 'react-redux';
 import {fetchStream} from '../../actions';
 
 class StreamShow extends Component{
+  constructor(props){
+    super(props);
+
+    this.videoRef = React.createRef();
+  }
+
   componentDidMount(){
     const id = this.props.match.params.id;
     this.props.fetchStream(id);
+
+    this.buildPlayer();
+  }
+
+  componentDidUpdate(){
+    this.buildPlayer();
+  }
+
+  componentWillUnmount(){
+    this.player.destroy();
+  }
+
+  buildPlayer(){
+    if(this.player || !this.props.stream){
+      return;
+    }
+
+    const id = this.props.match.params.id;
+
+    this.player = flv.createPlayer({
+      type: 'flv',
+      url: 'http://localhost:8000/live/' + id + '.flv'
+    });
+
+    this.player.attachMediaElement(this.videoRef.current);
+    this.player.load();
+    this.player.play();
   }
 
   render(){
@@ -18,6 +52,7 @@ class StreamShow extends Component{
 
     return(
       <div>
+        <video ref={this.videoRef} style={{width: '100%'}} controls/>
         <h1>{title}</h1>
         <h5>{description}</h5>
       </div>
