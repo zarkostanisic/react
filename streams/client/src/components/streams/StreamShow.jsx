4c0c55ea3c8249fc8fd32c0457c814288a2eafcc
zarko stanisic <zarko@tcom.rs>
2019-09-11 09:40:02 +0200

    modern-react-redux section22-4

--- streams/client/src/components/streams/StreamShow.jsx
+++ streams/client/src/components/streams/StreamShow.jsx
@@ -1,13 +1,38 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {fetchStream} from '../../actions';
 
 class StreamShow extends Component{
+  componentDidMount(){
+    const id = this.props.match.params.id;
+    this.props.fetchStream(id);
+  }
+
   render(){
+
+    if(!this.props.stream){
+      return <div>Loading...</div>;
+    }
+
+    const {title, description} = this.props.stream;
+
     return(
       <div>
-        StreamShow
+        <h1>{title}</h1>
+        <h5>{description}</h5>
       </div>
     );
   }
 }
 
-export default StreamShow;
+const mapStateToProps = (state, ownProps) => {
+  const id = ownProps.match.params.id;
+
+  return {
+    stream: state.streams[id]
+  }
+}
+
+export default connect(mapStateToProps, {
+  fetchStream: fetchStream
+})(StreamShow);
