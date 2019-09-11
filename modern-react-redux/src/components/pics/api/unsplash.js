28343812191de546b4d9d753b66589ab8f5fb483
zarko stanisic <zarko@tcom.rs>
2019-09-11 10:57:39 +0200

    modern-react-redux remove

--- modern-react-redux/src/components/pics/api/unsplash.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-export default axios.create({
-  baseURL: 'https://api.unsplash.com',
-  headers: {
-    Authorization: 'Client-ID bd0dd96af7d1d1139642a9c2150aedb428f640a698d717ae257b7e9dcab998d9'
-  }
-});
