8b976481e9560647d42d98fe5f2e1c4864608c59
zarko stanisic <zarko@tcom.rs>
2019-09-17 15:29:32 +0200

    modern-react-redux section26-6

--- modern-react-redux/modern-react-redux/src/components/seasons-hooks/App.jsx
+++ modern-react-redux/modern-react-redux/src/components/seasons-hooks/App.jsx
@@ -1,17 +1,11 @@
-import React, {useState, useEffect} from 'react';
+import React from 'react';
 import SeasonDisplay from './SeasonDisplay'
 import Spinner from './Spinner'
 
-const App = () => {
-  const [lat, setLat] = useState(null);
-  const [errorMessage, setErrorMessage] = useState('');
+import useLocation from './useLocation';
 
-  useEffect(() => {
-    window.navigator.geolocation.getCurrentPosition(
-      (position) => setLat(position.coords.latitude),
-      (err) => setErrorMessage(err.message)
-    );
-  }, []);
+const App = () => {
+  const [lat, errorMessage] = useLocation();
 
   let content;
 
