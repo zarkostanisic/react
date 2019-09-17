b924eb2fdf60e97d0b67afc8b2f0ffcd9100366e
zarko stanisic <zarko@tcom.rs>
2019-09-17 14:56:13 +0200

    modern-react-redux section25-16

--- modern-react-redux/hooks-simple/src/components/ResourceList.jsx
+++ modern-react-redux/hooks-simple/src/components/ResourceList.jsx
@@ -1,21 +1,16 @@
 import React, {Component, useState, useEffect} from 'react';
-import axios from 'axios';
+import useResources from './useResources';
+
 
 const ResourceList = ({resource}) => {
-  const [resources, setResources] = useState([]);
-
-  const fetchResource = async (resource) => {
-    const response = await axios.get('https://jsonplaceholder.typicode.com/' + resource);
-
-    setResources(response.data);
-  }
-
-  useEffect(() => {
-    fetchResource(resource);
-  }, [resource]);
+  const resources = useResources(resource);
 
   return(
-      <div>{resources.length}</div>
+      <ul>
+        {resources.map(record => (
+          <li key={record.id}>{record.title}</li>
+        ))}
+      </ul>
   );
 }
 
